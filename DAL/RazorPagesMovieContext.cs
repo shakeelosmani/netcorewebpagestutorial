20a407f5368056dce6dd517cf8b8e9f3beab4927
Shakeel Osmani <shakeel.osmani@gmail.com>
2018-12-16 01:08:12 -0500

    Initial check-in of module RazorPagesMovie

---
+++ DAL/RazorPagesMovieContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie.Models;
+
+namespace RazorPagesMovie.DAL
+{
+    public class RazorPagesMovieContext : DbContext
+    {
+        public RazorPagesMovieContext(DbContextOptions<RazorPagesMovieContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
