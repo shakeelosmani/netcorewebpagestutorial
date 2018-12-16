b175b93ec57408ce8429b0588526945efc0d1a69
Shakeel Osmani <shakeel.osmani@gmail.com>
2018-12-16 01:50:26 -0500

    add a link sanitize the display name for ReleasedDate

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -9,12 +9,15 @@ namespace RazorPagesMovie.Models
 
         public string Title { get; set; }
 
+
+        [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
 
         public string Genre { get; set; }
 
         public decimal Price { get; set; }
+
 
     }
 }
